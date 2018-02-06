7cd110ffd59f9769e324eca0f975f612985e3607
Rintaro Kinoshita <rintarou_17n1101186@nnn.ed.jp>
2018-02-06 02:59:17 +0000

    新規追加

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+'use scrict';
+const Twitter = require('twitter');
+
+const client = new Twitter({
+    consumer_key: 'eVYvOfWbZgcug4NBK2bhIBi5L',
+    consumer_secret: 'KkgIkR0QKRWhk8o9yDonGeoemoJt2rulgovmDUKM3qijz89JI2',
+    access_token_key: '960387461674250241-HRyML3QWoLVdDNxkYCEOTOhD5LcgEF2',
+    access_token_secret: 'er7Zrcmaur7pUa8GWaAfFKn1bGgDslQv6ZgVcuoMMOkvq'
+});
+
+module.exports = (robot) => {
+    robot.hear(/ポケモン>/i, (msg) => {
+        client.get('statuses/home_timeline', {}, function (error, tweets, response) {
+            msg.send(tweets);
+        });
+    });
+};
