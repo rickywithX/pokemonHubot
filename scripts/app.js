21d8b9a5da365b687b8302a94fd539359d553481
Rintaro Kinoshita <rintarou_17n1101186@nnn.ed.jp>
2018-02-09 03:09:45 +0000

    機能追加

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,35 @@
 'use scrict';
 const Twitter = require('twitter');
+
+// const client = new Twitter({
+//   consumer_key: process.env.TWITTER_API_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_API_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_API_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_API_ACCESS_TOKEN_SECRET
+// });
+
 const client = new Twitter({
-    consumer_key: 'eVYvOfWbZgcug4NBK2bhIBi5L',
-    consumer_secret: 'KkgIkR0QKRWhk8o9yDonGeoemoJt2rulgovmDUKM3qijz89JI2',
-    access_token_key: '960387461674250241-HRyML3QWoLVdDNxkYCEOTOhD5LcgEF2',
-    access_token_secret: 'er7Zrcmaur7pUa8GWaAfFKn1bGgDslQv6ZgVcuoMMOkvq'
+  consumer_key:'eVYvOfWbZgcug4NBK2bhIBi5L',
+  consumer_secret:'KkgIkR0QKRWhk8o9yDonGeoemoJt2rulgovmDUKM3qijz89JI2',
+  access_token_key:'960387461674250241-HRyML3QWoLVdDNxkYCEOTOhD5LcgEF2',
+  access_token_secret:'er7Zrcmaur7pUa8GWaAfFKn1bGgDslQv6ZgVcuoMMOkvq'
 });
 
+function getHomeTimeLine() {
+  client.get('statuses/home_timeline', {}, function(error, tweets, response) {
+      if(error) console.log(error);
+      console.log(tweets);
+  });
+}
+
+let checkedTweets = [];
 module.exports = (robot) => {
-    robot.respond(/twitter/i, (msg) => {
-        client.get('statuses/home_timeline', {}, function (error, tweets, response) {
-            if (error) msg.send(error);
-            msg.send(tweets);
-        });
+  robot.hear(/ポケモン同行会の皆さん！ポケモン公式twitterの情報を持ってきますよ！ちなみにこの文章をコピー＆ペーストしてこのチャンネルの貼り付けても、公式twitterの新しいツイートを持ってくることができますよ！/i, (msg) => {
+    client.get('statuses/home_timeline', {}, function (error, tweets, response) {
+      tweets.forEach(function(homeTimeLineTweet, key){
+        checkedTweets.push(homeTimeLineTweet.text);
+      });
+      msg.send(checkedTweets[0]);
     });
+  });
 };
---
+++ scripts/useful.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = (robot) => {
+    //メンションをつけて指定の単語を入力するとそれに応じたURLを提供してくれる
+    robot.respond(/公式/i, (msg) => {
+        msg.send('ポケットモンスターオフィシャルサイトです！' + ' \n' + 'http://www.pokemon.co.jp/');
+    });
+    robot.respond(/徹底/i, (msg) => {
+        msg.send('ポケモン徹底攻略です!' + ' \n' + 'https://yakkun.com/');
+    });
+    robot.respond(/攻略/i, (msg) => {
+        msg.send('ポケモン徹底攻略です!' + ' \n' + 'https://yakkun.com/');
+    });
+    robot.respond(/GO/i, (msg) => {
+        msg.send('ポケモンGO公式サイトです!' + ' \n' + 'http://www.pokemongo.jp/');
+    });
+    robot.respond(/天国/i, (msg) => {
+        msg.send('トレーナー天国です!' + ' \n' + 'https://pokemon-trainer.net/');
+    });
+    robot.respond(/ダメージ/i, (msg) => {
+        msg.send('ダメージ計算機です!' + ' \n' + 'https://pokemon-trainer.net/sm/dmcs/');
+    });
+    robot.respond(/計算/i, (msg) => {
+        msg.send('ダメージ計算機です!' + ' \n' + 'https://pokemon-trainer.net/sm/dmcs/');
+    });
+    robot.respond(/pgl/i, (msg) => {
+        msg.send('ポケモングローバルリンクです!' + ' \n' + 'https://3ds.pokemon-gl.com/');
+    });
+    robot.respond(/PGL/i, (msg) => {
+        msg.send('ポケモングローバルリンクです!' + ' \n' + 'https://3ds.pokemon-gl.com/');
+    });
+    robot.respond(/ものおき/i, (msg) => {
+        msg.send('ポケモンものおきです!' + ' \n' + 'http://pkc.client.jp/');
+    });
+    //botの使い方
+    robot.respond(/ポケモン/i, (msg) => {
+        const message = `こんにちは!! このbotができることを紹介します!!\n・1時間に一回、ポケモン公式ツイッターの最新ツイートをこのチャンネルに呟きます。\n・このbotにメンションをかけながら、特定の単語を入力すると、その単語にあったURLを教えてくれます。\n     特定の単語[公式,攻略,GO,天国,計算,pgl,ものおき]`
+        msg.send(message);
+    });
+    //部屋に入ったユーザーへの案内
+    robot.enter((msg) => {
+        const username = msg.message.user.profile.display_name;
+        const message = `こんにちは!!${username}さん!! ポケモン同好会へようこそ！ このbotができることを紹介します!!\n・1時間に一回、ポケモン公式ツイッターの最新ツイートをこのチャンネルに呟きます。\n・このbotにメンションをかけながら、特定の単語を入力すると、その単語にあったURLを教えてくれます。\n     特定の単語[公式,攻略,GO,天国,計算,pgl,ものおき]`
+        msg.send(message);
+    });
+};
