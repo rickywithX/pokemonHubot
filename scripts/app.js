0fdd1c879a9107f4400c4bf43d9a4ef129ba24c4
Rintaro Kinoshita <rintarou_17n1101186@nnn.ed.jp>
2018-02-20 02:27:07 +0000

    ファイルを分けた

--- scripts/app.js
+++ scripts/app.js
@@ -36,8 +36,3 @@ module.exports = (robot) => {
   });
 };
 
-module.exports = (robot) => {
-  robot.hear(/動作を起こす為に10分に一回、この発言を受けてこのbotが発言します。この文章をコピー＆ペーストでも動作確認を行います。/i, (msg) => {
-    msg.send("このbotは正常に作動しています。");
-  });
-};
